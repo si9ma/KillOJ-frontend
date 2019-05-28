878bf904da83ddedd351f796a8a0d53fac5bbf74
si9ma <hellob374@gmail.com>
2019-05-28 21:43:20 +0800

    delete unused code

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,6 @@
-import BaseInput from "./Inputs/BaseInput.vue";
 
-import BaseCheckbox from "./BaseCheckbox.vue";
-import BaseRadio from "./BaseRadio.vue";
 import BaseDropdown from "./BaseDropdown.vue";
-import BaseTable from "./BaseTable.vue";
 import BaseButton from "./BaseButton";
-import BaseAlert from "./BaseAlert";
-import BaseNav from "./BaseNav";
-import Modal from "./Modal";
-import CloseButton from "./CloseButton";
 import SplitPanel from "./SplitPanel";
 import Comment from "./Comment";
 import SubmitStatistic from "./SubmitStatistic";
@@ -22,23 +14,15 @@ import Card from "./Cards/Card.vue";
 import SidebarPlugin from "./SidebarPlugin/index";
 
 export {
-  BaseInput,
   Card,
-  Modal,
-  CloseButton,
   LanguageStatistic,
   SubmitList,
   ContributeBoard,
-  BaseTable,
   SubmitRank,
   Comment,
   SubmitStatistic,
-  BaseCheckbox,
-  BaseRadio,
   BaseDropdown,
   BaseButton,
   SplitPanel,
-  BaseAlert,
   SidebarPlugin,
-  BaseNav
 };
