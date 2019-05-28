1667b33fbb8ed5bf004fef4fa8bf2c68a33dc20f
si9ma <hellob374@gmail.com>
2019-05-28 21:47:17 +0800

    restore user.vue

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -86,6 +86,15 @@
                        :total="users.length">
         </el-pagination>
       </div>
+
+      <div class="col-4" v-if="sidebar">
+        <card v-if="activeUser.role !== 0">
+          <h5 slot="header">操作</h5>
+          <el-button size="mini" type="primary" @click="updateMaintainer()">{{activeUser.role === 1 ? '取消该用户的维护权限' : '将该用户设置为维护人员'}}</el-button>
+        </card>
+        <!-- user info -->
+        <user-card v-if="activeUser" :user="activeUser" title="用户信息"></user-card>
+      </div>
     </div>
   </div>
 </template>
@@ -278,6 +287,37 @@
         this.$refs.userTable.clearFilter();
         this.filterChange({})
       },
+      updateMaintainer() {
+        let role = this.activeUser.role === 1 ? 2 : 1
+        this.$axios({
+          url: this.$gbl.apiURL + '/admin/maintainers/' + this.activeUser.id,
+          method: 'put',
+          headers: AuthHeader(),
+          data: {
+            role
+          }
+        }).then(() => {
+          let foundIndex = this.users.findIndex(x => x.id === this.activeUser.id);
+          this.users[foundIndex].role = role
+          if (role === 1) {
+            this.users[foundIndex].tags = [{name:'Maintainer'}]
+          }else {
+            this.users[foundIndex].tags = []
+          }
+
+          this.$gbl.alert('success','设置成功')
+          console.log('update maintainer success')
+        }).catch(error => {
+          let json = ExtractJson(error.response)
+          if (json) {
+            this.$gbl.alert('danger',json.error.message)
+            console.log(json)
+          }else {
+            this.$gbl.alert('danger','设置出错')
+            console.log(error)
+          }
+        })
+      }
     },
   }
 </script>
